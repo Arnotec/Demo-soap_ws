960dddf51ddff81d2f161e0d0618897923646d65
Arnaud <tsofackarnaud@gmail.com>
2021-10-25 08:17:22 +0400

    Soap WS - using JaxB annotations to transient an attribute

--- src/main/java/org/arnotec/core/Account.java
+++ src/main/java/org/arnotec/core/Account.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Account {
     private Long code;
     private double solde;
+    @XmlTransient
     private Date dateCreation;
 }
